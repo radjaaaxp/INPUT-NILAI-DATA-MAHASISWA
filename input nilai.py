317eba044e256ab5f5c496e4e797b4fb86d306bd
Min-jaa 氷 <119745631+radjaaaxp@users.noreply.github.com>
2026-03-13 20:28:58 +0700

    Create input nilai.py

---
+++ input nilai.py
@@ -0,0 +1,25 @@
+nama = input("Nama: ")
+nim = input("NIM: ")
+prodi = input("Prodi: ")
+nilai = int(input("Nilai: "))
+
+if nilai >= 90:
+  grade ="A"
+elif nilai >= 80:
+  grade ="B+"
+elif nilai >= 70:
+  grade ="B"
+elif nilai >= 60:
+  grade ="C+"
+elif nilai >= 50:
+  grade ="C"
+elif nilai >= 40:
+  grade ="D"
+else:
+  grade ="E"
+
+print("\n--- DATA MAHASISWA ---")
+print(f"Nama: {nama}")
+print(f"NIM: {nim}")
+print(f"Prodi: {prodi}")
+print(f"Nilai: {grade}")
